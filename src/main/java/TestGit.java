8363ad1e48c57cfaed1487826edf35b953de011f
nn706 <1638620731@qq.com>
2020-03-21 14:16:36 +0800

    tinghao

--- src/main/java/TestGit.java
+++ src/main/java/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
         System.out.println("加油 加油 加油");
         System.out.println("北京加油 武汉加油 意大利加油");
         System.out.println("优秀");
+        System.out.println("挺好");
     }
 }
